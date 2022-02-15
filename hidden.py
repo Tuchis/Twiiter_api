f2c96d46ae94c559da397ef2b7cae82afaf25cb2
Vladyslav Humennyy <54081501+Tuchis@users.noreply.github.com>
2022-02-15 10:23:17 +0200

    LAB_2_2
    Downloaded all files, including data

---
+++ hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "mVnl5t...eoZkDDC",
+            "consumer_secret": "fS3FgJbOz...3HiNC7lUF5i",
+            "token_key": "941392...t6uibJfpJIr",
+            "token_secret": "efOxPW...jYsTPJE"}
