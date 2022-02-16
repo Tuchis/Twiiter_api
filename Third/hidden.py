c4aaea54d01a49e0524b2b888aecef7823920972
Vladyslav Humennyy <54081501+Tuchis@users.noreply.github.com>
2022-02-16 22:25:02 +0200

    Update hidden.py

--- Third/hidden.py
+++ Third/hidden.py
@@ -4,7 +4,7 @@
 # Create new App and get the four strings
 
 def oauth():
-    return {"consumer_key": "...",
-            "consumer_secret": "...",
-            "token_key": "...",
-            "token_secret": "..."}
+    return {"consumer_key": "mVnl5t...eoZkDDC",
+            "consumer_secret": "fS3FgJbOz...3HiNC7lUF5i",
+            "token_key": "941392...t6uibJfpJIr",
+            "token_secret": "efOxPW...jYsTPJE"}
